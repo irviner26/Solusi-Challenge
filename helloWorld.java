42a3dfe83b848a265b62603ef97e749bcbb3710c
Irvine <irvineringgo90@gmail.com>
2023-08-14 22:24:49 +0700

    Hello World

---
+++ helloWorld.java
@@ -0,0 +1,11 @@
+public class helloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+}
+
+/* Cara compile:
+	1. javac [namafile].java
+	2. java [namafile]
+	3. Nama file harus sama dengan nama class
+*/
