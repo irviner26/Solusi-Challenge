aaef55ea6453a261152d767e82e08050a6cef04c
Irvine <irvineringgo90@gmail.com>
2023-10-21 21:33:52 +0700

    Challenge 5 Push 1

--- challenge5/src/main/java/org/binaracademy/challenge4/repository/DetailRepository.java
+++
@@ -1,9 +0,0 @@
-package org.binaracademy.challenge4.repository;
-
-import org.binaracademy.challenge4.model.Detail;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DetailRepository extends JpaRepository<Detail, String> {
-}
